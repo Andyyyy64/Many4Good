cf2a07c14b38002d9ccea79c079f30a63b40f06f
Andyyyy64 <takuyarossi77@gmail.com>
2022-12-22 11:49:27 +0900

    okasikunarisou

--- front/src/components/Home.tsx
+++ front/src/components/Home.tsx
@@ -46,19 +46,6 @@ export default function Home() {
   const now: number = new Date();
   const months: number = now.getMonth() + 1; 
   const [selectmonth,setmonth] = useState<number>(months);
-
-
-  const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
   
   useEffect(() => {
     fetchAcountingData();
