1feed7abbc6f77bc0e05e23af40fa469802fe42f
Andyyyy64 <takuyarossi77@gmail.com>
2023-01-24 17:53:57 +0900

    aaa

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Auth0Provider
     domain={import.meta.env.VITE_APP_DOMEIN}
     clientId={import.meta.env.VITE_APP_CLIENT_ID}
     //@ts-ignore
-    redirectUri={window.location.origin}
+    redirectUri={"https://many4good.vercel.app"}
   >
     <App />
   </Auth0Provider>
