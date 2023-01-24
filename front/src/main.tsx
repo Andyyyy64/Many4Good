e86202d471e86f194d355b9a78d9a03420247f19
Andyyyy64 <takuyarossi77@gmail.com>
2023-01-24 17:40:01 +0900

    tsignore

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -5,8 +5,8 @@ import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  //@ts-ignore
   <Auth0Provider
+    //@ts-ignore
     domain={import.meta.env.VITE_APP_DOMEIN}
     clientId={import.meta.env.VITE_APP_CLIENT_ID}
     redirectUri={window.location.origin}
