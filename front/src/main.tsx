1a35eb9bd4faacfc8b765a6d1a60cb721138c090
Andyyyy64 <takuyarossi77@gmail.com>
2023-01-24 17:56:21 +0900

    aaa

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -7,7 +7,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Auth0Provider
     domain={import.meta.env.VITE_APP_DOMEIN}
     clientId={import.meta.env.VITE_APP_CLIENT_ID}
-    //@ts-ignore
     redirectUri={"https://many4good.vercel.app"}
   >
     <App />
